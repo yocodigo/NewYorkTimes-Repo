5c86aeee34b0fa50a192f887208db272c43bb461
unknown <jjhernandez94@gmail.com>
2018-06-20 10:29:24 -0700

    Response and aesthetic fixes

--- nytimes.js
+++ nytimes.js
@@ -18,7 +18,7 @@ $( document ).ready(function() {
         let article = $('#articleInput').val();
         const select = $('select').val();
 
-        // console.log(article);
+        // console.log($("#label").html());
         // console.log(beginDate);
         // console.log(endDate);
         // console.log(select);
@@ -38,10 +38,11 @@ $( document ).ready(function() {
             for(let i = 0; i <= select -1; i++) {
                 let div1 = $("<div>");
                     div1.attr("class", "card");
-                    div1.attr("style", "width: 18rem");    
+                    div1.attr("style", "width: 30rem"); 
+                    div1.attr("style", "margin: 30px, 40%");   
                 let img1 = $("<img>");
                     img1.attr("class", "card-img-top");
-                    img1.attr("src", "https://static01.nyt.com/" + result.response.docs[i].multimedia[2].url);
+                    img1.attr("src", "https://static01.nyt.com/" + result.response.docs[i].multimedia[1].url);
                     img1.attr("alt", "Card image cap");
                 
                 div1.append(img1);
@@ -51,20 +52,22 @@ $( document ).ready(function() {
                 
                 let title = $("<h5>");
                     title.attr("class", "card-title");    
-                    title.val(result.response.docs[i].headline.main);
+                    title.html(result.response.docs[i].headline.main);
                 
                 div2.append(title);
 
                 let summary = $("<p>");
                     summary.attr("class", "card-text");
-                    summary.val(result.response.docs[i].snippet);
+                    summary.attr("style", "text-align: left");
+                    summary.html(result.response.docs[i].snippet);
                 
                 div2.append(summary);
 
                 let site = $("<a>");
                     site.attr("href", result.response.docs[i].web_url);
                     site.attr("class", "btn btn-primary");
-                    site.val("Go to Page");
+                    site.attr("target", "_blank");
+                    site.html("Go to Page");
 
                 div2.append(site);
                 
