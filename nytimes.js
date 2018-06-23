f0facc2afeac1ae5d76277033eab2b6c8841e56d
unknown <jjhernandez94@gmail.com>
2018-06-22 18:58:11 -0700

    removed comments

--- nytimes.js
+++ nytimes.js
@@ -7,16 +7,6 @@ $( document ).ready(function() {
             $('.navbar').css('opacity', 1);
           }
         });
-      
-    // var date_input=$('input[name="date"]'); //our date input has the name "date"
-    //     var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
-    //     date_input.datepicker({
-    //         format: 'mm/dd/yyyy',
-    //         container: container,
-    //         todayHighlight: true,
-    //         autoclose: true,
-    //     })
-    //     date_input.datepicker(options);
     
     $(".btn").on("click", function() {
         
